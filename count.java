0a92cb1c680ca1459d5147a18a6827df91075029
Balakrishnan <164458422+bala2004end@users.noreply.github.com>
2025-07-31 15:39:58 +0530

    Create count.java
    count_digit

---
+++ count.java
@@ -0,0 +1,27 @@
+/*Count the number of digits in a given number
+
+Sample Input 0
+
+12345
+Sample Output 0
+
+5
+*/
+
+---------------------------------------𝐀𝐧𝐬𝐰𝐞𝐫-----------------------------------------------------
+import java.util.*;
+
+public class count {
+
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        long n=s.nextLong();
+        int count=0;
+        while(n>0){
+            count++;
+            n=n/10;
+        }
+        System.out.println(count);
+       
+    }
+}
