2bcc90179e66e94153c0c79bb796c4fb3f5cfd4c
Balakrishnan <balakrishnanm22cs@psnacet.edu.in>
2025-08-02 07:00:08 +0530

    solved

---
+++ value_or_not.java
@@ -0,0 +1,44 @@
+/*Write a C program to check the values are equal or not
+
+Input Format
+
+Accept two integer as input
+
+Constraints
+
+NIL
+
+Output Format
+
+EQUAL/NOT EQUAL
+
+Sample Input 0
+
+10 10
+Sample Output 0
+
+EQUAL
+Sample Input 1
+
+-109   83839
+Sample Output 1
+
+NOT EQUAL
+
+-------------------------------Answer------------------------------*/
+
+import java.util.*;
+
+public class value_or_not {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        if(a==b)
+        System.out.println("EQUAL");
+        else
+            System.out.println("NOT EQUAL");
+        
+    }
+}
