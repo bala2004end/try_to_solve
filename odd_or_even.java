546efffa11e39365be285b96c8839752bbb2cdea
Balakrishnan <balakrishnanm22cs@psnacet.edu.in>
2025-08-02 07:12:31 +0530

    check

---
+++ odd_or_even.java
@@ -0,0 +1,43 @@
+/*Write a C program to check the number is odd or even using conditional construct
+
+Input Format
+
+Accept an integer as an input
+
+Constraints
+
+NIL
+
+Output Format
+
+ODD/EVEN
+
+Sample Input 0
+
+3
+Sample Output 0
+
+ODD
+Sample Input 1
+
+8
+Sample Output 1
+
+EVEN
+
+-----------------------------Answer----------------------------
+*/
+import java.util.*;
+
+public class odd_or_even {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a%2==0)
+            System.out.println("EVEN");
+        else
+            System.out.println("ODD");
+        
+    }
+}
